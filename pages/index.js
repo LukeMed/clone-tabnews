ca34c3e16161a4886f7f4331f89ceb408716059d
Lucas A. Medeiros <90503657+LukeMed@users.noreply.github.com>
2024-10-17 08:22:45 +0000

    Up date 17/10/2024

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     
     return <><h1>Fala, Lucas! Vamos retomar as aulas?</h1>
-    <h2>Continuo na batalha. Vamos usar mais comandos</h2></>
+    <h2>Continuo na batalha. Vamos usar mais comandos. Não deleta, GitHub</h2></>
 }
 
 export default Home;
