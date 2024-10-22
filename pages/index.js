b0a746698d2414f5948b564d7288c72ec8d65224
Lucas A. Medeiros <90503657+LukeMed@users.noreply.github.com>
2024-10-22 16:48:21 +0000

    Pequena alteração

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     
     return <><><h1>Fala, Lucas! Vamos retomar as aulas?</h1>
         <h2>Continuo na batalha. Vamos usar mais comandos. Não deleta, GitHub. Precisa treinar mais.</h2></>
-        <h3>Mais uma frase. Treinando na aula.</h3></>
+        <h3>Mais uma frase. Treinando na aula. Uma pequena alteração.</h3></>
 }
 
 export default Home;
