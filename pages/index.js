069ef1949ed7f3dacf640ef36a8d149f2c4ff0cc
Lucas A. Medeiros <90503657+LukeMed@users.noreply.github.com>
2024-10-29 18:25:32 +0000

    Add pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     
-    return <h1>Fala, família! Acabei de integrar o GitHub com a Vercel para poder hospedar um projeto, isto é, colocar o site no ar. Buenas!</h1>
+    return <h1>Fala, família! Acabei de integrar o GitHub com a Vercel para poder hospedar um projeto, isto é, colocar o site no ar. Buenas!!</h1>
 }
 
 export default Home;
