e588ee81cbb8ebb7ee5d3bb51f1b2535c006131c
Lucas A. Medeiros <90503657+LukeMed@users.noreply.github.com>
2025-07-10 17:45:50 +0000

    Atualização do 10/07/2025

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Vamos aprender desenvolver páginas Web? Isso será útil para a Transplam.</h1>
+    return <h1>Vamos aprender desenvolver páginas Web? Isso será útil para a Transplam. Dia 10/07/2025</h1>
 }
 
 export default Home;
