b423d78659d0249c73181d3aad03480f3225d733
Lucas A. Medeiros <90503657+LukeMed@users.noreply.github.com>
2025-07-25 02:20:10 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Vamos aprender desenvolver páginas Web? Isso será útil para a Transplam. Dia 10/07/2025</h1>
+function Home() {
+  return (
+    <h1>
+      Vamos aprender desenvolver páginas Web? Isso será útil para a Transplam.
+      Dia 24/07/2025
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
