c40484985638cb18176428be2fc8807dbdd05435
Lucas A. Medeiros <90503657+LukeMed@users.noreply.github.com>
2025-09-25 23:31:18 +0000

    25/09/2025 pra não esquecer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Mais um teste. Esse é do dia 25/09/2025</h1>;
+  return (
+    <h1>
+      Mais um teste. Esse é do dia 25/09/2025. Vamos estudar pra não esquecer.
+    </h1>
+  );
 }
 
 export default Home;
